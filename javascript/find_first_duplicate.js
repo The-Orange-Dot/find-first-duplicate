b9de2f80a47f475056c6aaddfdf6b4243c7d1253
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-24 20:02:33 +0900

    Completed

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,5 +1,19 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  const unique = new Set(arr);
+
+  console.log(unique);
+
+  unique.forEach((setNum) => {
+    if (arr.includes(setNum)) {
+      arr.shift(setNum);
+      unique.delete(setNum);
+    } else {
+      null;
+    }
+    console.log(arr);
+  });
+
+  return arr.length === 0 ? -1 : arr[0];
 }
 
 if (require.main === module) {
@@ -11,9 +25,29 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log("=>", findFirstDuplicate([1, 2, 3, 4]));
+
+  console.log("");
+
+  console.log("Expecting: 8");
+  console.log("=>", findFirstDuplicate([6, 2, 6, 8, 2, 8]));
+
+  console.log("");
+
+  console.log("Expecting: -1");
+  console.log("=>", findFirstDuplicate([9, 3, 5, 1, 0, 2]));
 }
 
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//The new Set stores the Array's unique value first.
+
+//When looping, we're using the array's first value to loop through the array and deleting the first value it finds.
+//If there is only one value in the array, that value will no longer exist. It would also delete the same value from the Set too.
+
+//It would continue this loop until there are no more unique values in the Array, and no more values in the Set, leaving only the duplicate values.
+
+//After, the function uses a ternary statement to return a "-1" if there is nothing in the array (no unique values)
+//but return the first value of the Array if there are any duplicates
